d4717c0baccb9ba573ed2ff8cd29c443a17cdbfa
Lukas Plachy <lukas@plachy.eu>
2025-12-08 23:35:40 +0100

    Try list-table class for selection panel background

--- listedit/src/views/editor.php
+++ listedit/src/views/editor.php
@@ -33,7 +33,7 @@ $this->addCssFile('modules/listedit/assets/css/listedit.css');
     <!-- Selection Panels Container - Side by Side -->
     <div class="selection-panels-container">
         <!-- Host/Template Selection Panel -->
-        <div class="selection-panel selection-panel-host ui-widget-content ui-corner-all">
+        <div class="selection-panel selection-panel-host list-table">
             <label class="panel-label">1. Select Host or Template</label>
             <div class="selection-content">
                 <div id="current-host-display" class="selection-display">Not selected</div>
@@ -42,7 +42,7 @@ $this->addCssFile('modules/listedit/assets/css/listedit.css');
         </div>
         
         <!-- Macro Selector Panel -->
-        <div class="selection-panel selection-panel-macro ui-widget-content ui-corner-all">
+        <div class="selection-panel selection-panel-macro list-table">
             <label class="panel-label">2. Select Macro to Edit</label>
             <div class="selection-content">
                 <select id="macro-selector" class="macro-selector">
